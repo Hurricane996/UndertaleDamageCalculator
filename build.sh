4edab7f0f448c25b9045a0ed331fb17c5d3ace9e
Hurricane996 <levijwillrich@gmail.com>
2021-07-20 22:59:44 -0500

    restructuring

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 mkdir -p dist && 
+cp -r public/* dist/ &&
 babel  src -d dist --source-maps --extensions ".ts,.js" && 
 cp src/index.html dist/index.nomin.html && 
-sass src/index.scss:dist/index.css --style compressed && 
+sass src/css/index.scss:dist/css/index.css --style compressed --embed-sources && 
 cp src/manifest.json dist/manifest.json && 
 pwa-asset-generator src/icon.svg dist/ --favicon --manifest dist/manifest.json --index dist/index.nomin.html --icon-only
 minify dist/index.nomin.html > dist/index.html &&
--- src/js/registerServiceWorker.js
+++ src/js/registerServiceWorker.js
@@ -1,6 +1,6 @@
 if('serviceWorker' in navigator) {
     window.addEventListener("load", () => {
-        navigator.serviceWorker.register("/sw.js").then(
+        navigator.serviceWorker.register("/js/sw.js").then(
             registration => console.log(`Successfully registered service worker ${registration}`), 
             err => console.log(`Failed to register service worker: ${err}`)
         );
